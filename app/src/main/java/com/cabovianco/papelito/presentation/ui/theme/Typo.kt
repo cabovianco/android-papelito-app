e15119443acc21230b31f1a39e394531a80543bf
thomas <github@cabovianco.anonaddy.me>
2025-12-07 15:11:01 -0300

    Add fonts

---
+++ app/src/main/java/com/cabovianco/papelito/presentation/ui/theme/Typo.kt
@@ -0,0 +1,17 @@
+package com.cabovianco.papelito.presentation.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.cabovianco.papelito.R
+
+val BitcountGridDouble = FontFamily(
+    Font(R.font.bitcount_grid_double_variable)
+)
+
+val CherryBombOne = FontFamily(
+    Font(R.font.cherry_bomb_one_regular)
+)
+
+val Fascinate = FontFamily(
+    Font(R.font.fascinate_regular)
+)
