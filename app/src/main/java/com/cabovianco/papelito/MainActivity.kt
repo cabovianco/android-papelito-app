844e66fefd12bbc47e709cf73eb24974174dce99
thomas <github@cabovianco.anonaddy.me>
2025-12-07 14:46:37 -0300

    Fix: System bar color on system dark mode

--- app/src/main/java/com/cabovianco/papelito/MainActivity.kt
+++ app/src/main/java/com/cabovianco/papelito/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.cabovianco.papelito
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.cabovianco.papelito.presentation.navigation.AppNavigation
@@ -12,7 +14,13 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        val scrim = Color.rgb(59, 62, 67)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(scrim, scrim),
+            navigationBarStyle = SystemBarStyle.light(scrim, scrim)
+        )
+
         setContent {
             PapelitoTheme {
                 AppNavigation()
